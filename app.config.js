727b70c42270c7780897d04b4db6d93497278683
stangerjm <jamesstanger987@gmail.com>
2018-06-26 15:03:38 -0700

    Reformatted config for aesthetics

--- app.config.js
+++ app.config.js
@@ -1,51 +1,23 @@
 export const config = {
     nav:
       [
-        {
-          'itemTitle': 'Home',
-          'linkPath': '#'
-        },
-        {
-          'itemTitle': 'WSP',
-          'linkPath': '#'
-        },
-        {
-          'itemTitle': 'More',
-          'linkPath': '#',
+        { 'itemTitle': 'Home', 'linkPath': '#' },
+        { 'itemTitle': 'WSP', 'linkPath': '#' },
+        { 'itemTitle': 'More', 'linkPath': '#',
           'inlineItems':
             [
-              {
-                'itemTitle': 'Be a Trooper',
-                'linkPath':'#'
-              },
-              {
-                'itemTitle': 'Media',
-                'linkPath': '#'
-              }
+              { 'itemTitle': 'Be a Trooper','linkPath':'#' },
+              { 'itemTitle': 'Media', 'linkPath': '#' }
             ]
         },
-        {
-          'itemTitle': 'Links',
-          'linkPath': '#',
+        { 'itemTitle': 'Links', 'linkPath': '#',
           'inlineItems':
             [
-              {
-                'itemTitle': 'breathtest',
-                'linkPath': '#'
-              },
-              {
-                'itemTitle': 'wsp.wa.gov',
-                'linkPath': '#'
-              },
-              {
-                'itemTitle': 'Google',
-                'linkPath': '#'
-              }
+              { 'itemTitle': 'breathtest', 'linkPath': '#' },
+              { 'itemTitle': 'wsp.wa.gov', 'linkPath': '#' },
+              { 'itemTitle': 'Google', 'linkPath': '#' }
             ]
         },
-        {
-          'itemTitle': 'Contact',
-          'linkPath': '#'
-        }
+        { 'itemTitle': 'Contact', 'linkPath': '#'}
       ]
 };
