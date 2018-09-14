e491ea73098673c85e38f76e5b2219ed1a96d3c9
stangerjm <jamesstanger987@gmail.com>
2018-09-14 14:37:04 -0700

    Added inline documentation for toTitleCase mixin

--- src/global/mixins/toTitleCase.js
+++ src/global/mixins/toTitleCase.js
@@ -5,6 +5,8 @@
  */
 export function toTitleCase(text) {
   return text
+    // Separate words with spaces
     .replace(/([A-Z])/g, ' $1')
+    // Capitalize the first letter of each word
     .replace(/^./, (substr) => { return substr.toUpperCase(); });
 }
